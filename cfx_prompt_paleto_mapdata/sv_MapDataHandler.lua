36b7de21b52b06d56f276b2b94c239a21290ecb1
Vertex-Hub <vertexhub.fivem@gmail.com>
2026-02-07 19:08:24 +0000

    Update map data - Repair+Firestation

---
+++ cfx_prompt_paleto_mapdata/sv_MapDataHandler.lua
@@ -0,0 +1,28 @@
+local maps = {
+	"prompt_repair_shops",
+	"paleto_bay_fire_station"
+}
+
+local events = {}
+
+-- Paleto mapdata exists event
+RegisterNetEvent("prompt:mapdata_paleto_exists", function(cb)
+  cb(true)
+end)
+
+-- Paleto mapdata list event
+RegisterNetEvent("prompt:mapdata_paleto_sendList", function(returnevent)
+  TriggerEvent(returnevent, maps)
+end)
+
+-- Legacy support for individual map checks
+for i = 1, #maps do
+  local eventName = maps[i] .. ":mapDataExists"
+  if Debug == true then
+    print("Creating event: ", eventName)
+  end
+  local event = RegisterNetEvent(eventName, function(cb)
+    cb(true)
+  end)
+  table.insert(events, event)
+end
