628b777bbc84c021c50f2db99e4b354f4b8446a6
Vertex-Hub <vertexhub.fivem@gmail.com>
2026-08-15 20:16:01 +0000

    Update map data - Train+Repair+Sheriff+Firestation

---
+++ cfx_prompt_paleto_mapdata/sv_MapDataHandler.lua
@@ -0,0 +1,30 @@
+local maps = {
+	"prompt_trainstation_paleto",
+	"prompt_repair_shops",
+	"prompt_tuco_paleto_sheriff",
+	"paleto_bay_fire_station"
+}
+
+local events = {}
+
+-- Paleto mapdata exists event
+RegisterNetEvent("prompt:mapdata_paleto_exists", function(cb)
+  cb(true)
+end)
+
+-- Paleto mapdata list event
+RegisterNetEvent("prompt:mapdata_paleto_sendList", function(returnevent)
+  TriggerEvent(returnevent, maps)
+end)
+
+-- Legacy support for individual map checks
+for i = 1, #maps do
+  local eventName = maps[i] .. ":mapDataExists"
+  if Debug == true then
+    print("Creating event: ", eventName)
+  end
+  local event = RegisterNetEvent(eventName, function(cb)
+    cb(true)
+  end)
+  table.insert(events, event)
+end
